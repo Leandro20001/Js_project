0f6ea67f40af6cbc8f81d1cddd300968cec31cc6
Leandro20001 <leandroferreira.dev@gmail.com>
2024-06-22 16:00:07 -0300

    db connected

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,11 @@
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
 dotenv.config()
 
-const connection = await mysql.createConnection({
-    host:MYSQL_HOST,
-    user:MYSQL_USER,
-    password:MYSQL_PASSWORD,
-    database:MYSQL_DATABASE
-});
-try{
-    const[result] = await connection.query(
-        'SELECT * FROM employees'
-    )
-    return result
-
-} catch(err){
-    console.log(err)
-}
-
-connection.correct((err)=>{
-    if(err) throw err;
-    console.log("Conectado ;)")
+const connection = mysql.createPool({
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE,
 })
+
--- main.cjs
+++ main.cjs
@@ -1,23 +1,30 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
+dotenv.config()
 app.use(express.json())
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-    return res.sendFile(__dirname + '/main.html')
+const connection = mysql.createPool({
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE,
 })
+
+
+//rota de todos os empregados
+app.get('/',(req,res)=>{
+    let query = 'SELECT * FROM employees'
+    connection.query(query,(err,res)=>{
+        console.log(res)
+    }),res.sendFile(__dirname + '/main.html')
+});
+
+app.post('/',(req,res)=>{
+
+})
+
 app.listen(3333)
-console.log('ola')
-
-
-const response = async function(){
-    const info = await fetch('https://randomuser.me/api/').then(
-        console.log('usuario encontrado')
-    )
-    const user = await info.json().then((userN)=>{
-        console.log(userN.results)
-         
-    })
-}
-response()
 
