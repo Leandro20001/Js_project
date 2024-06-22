b9dd6b44a2b2022e0e342748cc5d786f94fa91db
Leandro20001 <leandroferreira.dev@gmail.com>
2024-06-22 12:14:03 -0300

    db update

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,19 @@
-const sql = require('mysql')
-const connection = mysql.createConnection({
-    host :"localhost",
-    user:"root",
-    password:"L.0987654321"
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const connection = await mysql.createConnection({
+    host : process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
 });
+//mostra todos os dados da tabela:
+try {
+    const[results] = await connection.query('SELECT *  FROM EMPLOYEE-DB')
+    console.log(results)
+
+} catch{console.log('falha na requisição...')}
+
 connection.correct((err)=>{
     if(err) throw err;
     console.log("Conectado ;)")
--- routes/home.js
+++
@@ -1,7 +0,0 @@
-
-const express = require('express')
-app.use(express.json())
-app.get('/',(req,res)=>{
-    return res.send('HOME')
-})
-app.listen(3000)
